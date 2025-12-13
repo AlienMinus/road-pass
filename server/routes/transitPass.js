d4102dbd1664cb24578212128d88e86acb076c8a
AlienMinus <dasmanasranjan2005@gmail.com>
2025-12-13 19:57:00 +0530

    DT fix

--- server/routes/transitPass.js
+++ server/routes/transitPass.js
@@ -8,15 +8,12 @@ router.post('/create', async (req, res) => {
     // Generate pass number in format: A25{MMDDHHMMSS}/1/{serial}
     const { passNo, serial } = await TransitPass.generatePassNo();
 
-    const fromDT = new Date(req.body.fromDT + 'Z');
-    const toDT = new Date(req.body.toDT + 'Z');
-
     const passData = {
       passNo: passNo,
       serial: serial,
       book: req.body.book,
-      fromDateTime: fromDT,
-      toDateTime: toDT,
+      fromDateTime: req.body.fromDT,
+      toDateTime: req.body.toDT,
       circle: 'BHADRAK',
       quarry: 'MANKIDIA - KA',
       licensee: 'SUSANTA KUMAR SAHOO',
